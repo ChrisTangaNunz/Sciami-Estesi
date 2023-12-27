d3f80b732d82c8aaa3af5ad7727e35ca90687891
ChrisTangaNunz <tangachris0@gmail.com>
2023-12-27 13:56:06 +0100

    First commit

---
+++ coinc_tel_123.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import time
+import numpy as np 
+
+
+# Registra il timestamp iniziale
+start_time = time.time()
+
+
+
+def coincidenza_doppia(tel1, tel2):
+    coincidenze = []
+
+    idx1, idx2 = 0, 0
+    len1, len2= len(tel1), len(tel2)
+
+    while idx1 < len1 and idx2 < len2:
+        time1, time2= tel1[idx1], tel2[idx2]
+        min_time = min(time1, time2)
+        max_time = max(time1, time2)
+
+        delta_1 = abs(max_time - min_time)
+
+        if delta_1 <= 200e-9 and delta_1 >= 150e-9:
+            coincidenze.append((time1, time2))
+
+        # Incrementa l'indice del telescopio con il tempo più basso
+        if min_time == time1:
+            idx1 += 1
+        elif min_time == time2:
+            idx2 += 1
+
+
+    return coincidenze
+
+
+def coincidenze(telescopio_master, telescopio1, telescopio2):
+    j=0
+    k=0
+    len1=len(telescopio1)
+    len2=len(telescopio2)
+    t1_max = 100e-9
+    t1_min = 50e-9
+    t2_max = 200e-9
+    t2_min = 150e-9
+
+    coincidenze=[]
+    
+    for i in range(len(telescopio_master)):
+        while ( telescopio1[j] < telescopio_master[i]+ t1_min  and j<len1): 
+            j+=1
+        if telescopio1[j] < telescopio_master[i]+ t1_max:
+            while ( telescopio2[k] < telescopio_master[i]+ t2_min  and k<len2): 
+                k+=1
+            if telescopio2[k] < telescopio_master[i]+ t2_max:
+                coincidenze.append((telescopio_master[i], telescopio1[j], telescopio2[k]))
+                j+=1  
+                k+=1
+    return coincidenze
+           
+           
+
+
+     
+
+def coincidenze_doppie( telescopio_master, telescopio2):
+    j=0
+    len1=len(telescopio2)
+    t1_max = 100e-9
+    t1_min = 50e-9
+    t2_max = 200e-9
+    t2_min = 150e-9
+
+
+    coincidenze=[]
+    
+    for i in range(len(telescopio_master)):
+        while (telescopio2[j] < telescopio_master[i]+ t2_min  and j<len1): 
+            j+=1            
+        if telescopio2[j] < telescopio_master[i]+ t2_max:
+            coincidenze.append((telescopio_master[i], telescopio2[j]))
+            j+=1
+               
+    return coincidenze
+             
+                
+ 
+                
+                                
+            
+
+                  
+    
+
+
+
+
+
+tempi_canale_1 = np.loadtxt("/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_1_231123.txt")
+tempi_canale_2 = np.loadtxt("/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_2_231123.txt")
+tempi_canale_3 = np.loadtxt("/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_3_231123.txt")
+
+#v = coincidenze(tempi_canale_3, tempi_canale_1, tempi_canale_2)
+doppie = coincidenze_doppie(tempi_canale_3, tempi_canale_2)
+triple = coincidenze(tempi_canale_3, tempi_canale_1, tempi_canale_2)
+
+#np.savetxt('/home/chris/SciamiEstesi/SciamiCoincidenza/coincidenze_triple.txt', v, fmt='%.8f')
+runnumber=231123
+np.savetxt(f'/home/chris/SciamiEstesi/23_11_2023/data/concidence_data/coincidenze_doppie23_{runnumber}.txt', doppie, fmt='%.8f')
+np.savetxt(f'/home/chris/SciamiEstesi/23_11_2023/data/concidence_data/coincidenze_triple_{runnumber}.txt', triple, fmt='%.8f')
+
+
+# Registra il timestamp finale
+end_time = time.time()
+
+# Calcola la differenza e stampa il tempo di esecuzione
+execution_time = end_time - start_time
+print(f"Tempo di esecuzione: {execution_time} secondi")
---
+++ newtime.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+
+def correzione_dati(tempi):
+    tempi_corretti = [tempi[0]]
+
+    for i in range(1, len(tempi)):
+        if tempi[i] > tempi[i - 1]:
+            tempi_corretti.append(tempi[i])
+        else:
+            diff = tempi[i - 1] - tempi[i]
+            for j in range(i, len(tempi)):
+                tempi[j] += diff
+            tempi_corretti.append(tempi[i])
+
+    tempi[:] = tempi_corretti
+    
+    
+    
+def correzione_dati2(tempi):
+    tempi_corretti = [tempi[0]]
+    corr = 0
+    delta_corr= 0 
+    for i in range(1, len(tempi)):
+        if tempi[i] < tempi[i - 1]:
+            diff = tempi[i - 1] - tempi[i]
+            corr=corr+diff+delta_corr
+              
+        tempi_corretti.append(tempi[i]+corr)
+
+    tempi[:] = tempi_corretti
+    
+
+# Caricamento dati
+runnumber=231123
+channel, time = np.loadtxt("/home/chris/SciamiEstesi/23_11_2023/data/231123test2.dat", unpack=True)
+
+
+
+correzione_dati2(time)
+
+
+# Separazione dei tempi in base al canale
+tempi_canale_1 = time[channel == 1]
+tempi_canale_2 = time[channel == 2]
+tempi_canale_3 = time[channel == 3]
+
+#np.savetxt('//home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_tot.txt', time)
+
+# Salvataggio dei tempi corretti per ogni canale
+np.savetxt(f'/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_1_{runnumber}.txt', tempi_canale_1, fmt='%.8f')
+np.savetxt(f'/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_2_{runnumber}.txt', tempi_canale_2, fmt='%.8f')
+np.savetxt(f'/home/chris/SciamiEstesi/23_11_2023/data/clean_data/tempi_corretti_canale_3_{runnumber}.txt', tempi_canale_3, fmt='%.8f')
+'''
+plt.scatter(range(len(tempi_canale_1)), tempi_canale_1, label='Canale 1')
+plt.scatter(range(len(tempi_canale_2)), tempi_canale_2, label='Canale 2')
+plt.scatter(range(len(tempi_canale_3)), tempi_canale_3, label='Canale 3')
+
+plt.xlabel('Indice')
+plt.ylabel('Tempi Corretti')
+plt.legend()
+plt.show()
+'''
